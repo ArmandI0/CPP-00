c71b1406fda3912d6a1a47fa64bc004901634106
Armand <aanger3@gmail.com>
2024-04-24 22:22:15 +0200

    ok

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Contact_class.hpp                                  :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: armandanger <armandanger@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/15 09:23:20 by armandanger       #+#    #+#             */
-/*   Updated: 2024/04/24 22:15:50 by armandanger      ###   ########.fr       */
+/*   Created: 2024/04/24 22:16:31 by armandanger       #+#    #+#             */
+/*   Updated: 2024/04/24 22:16:32 by armandanger      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CONTACT_CLASS_H
-# define CONTACT_CLASS_H
-# include <string>
-
-class Contact
-{
-	public:
-		Contact(void);
-		~Contact();
-	private:
-		std::string	firstname;
-		std::string	lastname;
-		std::string	nickname;
-		std::string	phone_number;
-		std::string	darkest_scrt;
-};
-
-#endif
